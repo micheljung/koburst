8afc5be7c929950ac03957417a7af89a0c487d7a
Michel Jung <michel.jung89@gmail.com>
2024-10-17 20:00:38 +0200

    Use git-versioning plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
-group = "io.koburst"
-version = "0.1.0-SNAPSHOT"
+plugins {
+  alias(libs.plugins.gitVersioning)
+}
 
+group = "io.koburst"
+version = "0.0.0-SNAPSHOT"
+gitVersioning.apply {
+  refs {
+    branch(".+") {
+      version = "\${ref.slug}-SNAPSHOT"
+    }
+    tag("(?<version>.*)") {
+      version = "\${ref.version}"
+    }
+  }
+  rev {
+    version = "\${commit}"
+  }
+}
